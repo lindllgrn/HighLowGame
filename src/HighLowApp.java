74b0af1bec08173f3ad55fd2f5e9224e662e072e
lindllgrn <lindllgrn@gmail.com>
2023-02-13 09:39:31 -0600

    Testing

--- src/HighLowApp.java
+++ src/HighLowApp.java
@@ -1,10 +1,103 @@
+import java.util.Scanner;
+
+/********************************************************************************************
+ Description: This takes the user's number of invoices and the prices and calculates them
+ into discounts, amounts, and averages.
+ Programmer: Lindsay Green
+ Date Written: 2023.01.27
+ GitHub: https://github.com/lindllgrn/JavaIndividualProjects/tree/main/section1-invoiceapp
+ ********************************************************************************************/
 
 public class HighLowApp {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("High/Low Game");
-		
-	}
+		// initialize variables for use in calculating averages
+		int invoiceCount = 0;
+		int lineItems = 0;
 
-}
+		double invoiceTotal = 0.0;
+		double discountTotal = 0.0;
+		double subtotal = 0.0;
+		double discountPercent = 0.0;
+		double discountAmount = 0.0;
+		double discountInvoiceTotal = 0.0;
+
+		String choice = "y";
+
+		// create a Scanner object named sc
+		Scanner sc = new Scanner(System.in);
+
+		// welcome the user to the program
+		System.out.println(); //print blank line
+		System.out.println("==========================================");
+		System.out.println("Welcome to the Invoice Total Calculator V5");
+		System.out.println("==========================================");
+
+		// perform invoice calculations until choice is "n" or "N"
+		while (!choice.equalsIgnoreCase("n")) {
+
+			// get the invoice line items from the user
+			System.out.println();
+			System.out.print("Enter the number of invoice line items: ");
+			lineItems = sc.nextInt();
+
+			System.out.println();
+			System.out.println("==========================================");
+			System.out.println();
+
+			//user enters the invoice amounts
+			for (int i = 1; i <= lineItems; i++) {   
+				System.out.print("Enter the #" + i + " line item amount:   ");
+				subtotal += sc.nextDouble();
+
+				System.out.println();
+			}
+
+			System.out.println("==========================================");
+
+			// calculate the discount amount and total
+			if (subtotal >= 500) {
+				discountPercent = 0.25;
+			} else if (subtotal >= 200) {
+				discountPercent = 0.2;
+			} else if (subtotal >= 100) {
+				discountPercent = 0.1;
+			} else {
+				discountPercent = 0.0;
+			}
+			discountAmount = subtotal * discountPercent;
+			discountInvoiceTotal = subtotal - discountAmount;
+
+			// accumulate the invoice count and invoice total
+			invoiceTotal = invoiceTotal + discountInvoiceTotal;
+			discountTotal = discountTotal + discountAmount;
+			invoiceCount = invoiceCount + 1;  
+
+			System.out.println();
+
+			//displays the user's proper numbers according to the input data
+
+			System.out.println();
+			System.out.println("==========================================");
+
+			choice = sc.nextLine(); //clear the return char still in the input buffer
+
+			System.out.println();
+
+			//asks the user if they would like to continue
+			System.out.print("Continue? (y/n): ");
+			choice = sc.nextLine();
+			System.out.println();
+		}
+
+		//caluculates and displays invoice count, average invoice, and average discount
+
+		//sends a goodbye message to the user when they are finished using the program
+		System.out.println("==========================================");
+		System.out.println();
+		System.out.println("Goodbye");
+		System.out.println();
+		System.out.println("==========================================");
+
+		sc.close();
+	} //end of main
+} //end of invoiceapp class
